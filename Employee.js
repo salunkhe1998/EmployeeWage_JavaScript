02dd511c33bf4ee3ffd6cde391f07cff3bd3f6c2
salunkhe1998 <117974213+salunkhe1998@users.noreply.github.com>
2023-05-06 09:14:10 +0530

    Employee present or Absent

---
+++ Employee.js
@@ -0,0 +1,11 @@
+//UC1- Check Employee Presnt or Absent
+
+
+const IS_ABSENT = 0
+    let Employee = Math.floor(Math.random() * 10) % 2;
+    
+    if (employeeCheck == IS_ABSENT) {
+        console.log("Employee is ABSENT");
+    } else {
+        console.log("Employee is PRESENT");
+    }
