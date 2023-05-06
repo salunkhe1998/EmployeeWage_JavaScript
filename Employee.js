1e2fef22ff5e5f510559d1bef8037241f40d9a0a
salunkhe1998 <117974213+salunkhe1998@users.noreply.github.com>
2023-05-06 09:20:43 +0530

    calculating employeewage

---
+++ Employee.js
@@ -0,0 +1,30 @@
+   //UC2- Calculating EmployeeWage
+   
+   
+const IS_PART_TIME = 1;
+const IS_FULLTIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+let employeeHours = 0;
+let employeeCheck = Math.floor(Math.random() * 10) % 3;
+
+switch (employeeCheck) {
+
+    case IS_PART_TIME:
+        employeeHours = PART_TIME_HOURS;
+        console.log("Employee works Part Time");
+        break;
+
+    case IS_FULL_TIME:
+        employeeHours = FULL_TIME_HOURS;
+        console.log("Employee works Full Time");
+        break;
+
+    default:
+        employeeHours = 0;
+}
+
+let employeeWage = employeeHours * WAGE_PER_HOUR;
+console.log("Employee Wage = " + employeeWage);
